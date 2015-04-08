f976f0ecc0402eb23f19a238db51cf6df6c5004c
Ethan Saaris <wolffe@users.noreply.github.com>
2015-04-08 14:45:11 +0100

    Update users.php

--- components/users.php
+++ components/users.php
@@ -1,27 +1,23 @@
 <?php
 function user_register($username, $password, $email) {
-  $db = new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME . ';charset=utf8', DBUSER, DBPASS);
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	nuukdb();
 
-    $password = hash_hmac('sha512', $password, '#+pT%B[M1X3R),Z(Y+q_tZwX~l@DXum2PdHS8hvmaVENrmI_?cvE#j8]n^.u]Ni+');
+	$password = hash_hmac('sha512', $password, '#+pT%B[M1X3R),Z(Y+q_tZwX~l@DXum2PdHS8hvmaVENrmI_?cvE#j8]n^.u]Ni+');
 
-    $stmt = $db->prepare("INSERT INTO user (username, password, email, registration_date) VALUES ('$username', '$password', '$email', NOW())");
-    $stmt->execute();
+	$stmt = $db->prepare("INSERT INTO user (username, password, email, registration_date) VALUES ('$username', '$password', '$email', NOW())");
+	$stmt->execute();
 }
 
 function user_login($username, $password) {
-	$db = new PDO('mysql:host=' . DBHOST . ';dbname=' . DBNAME . ';charset=utf8', DBUSER, DBPASS);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	nuukdb();
 
-    $password = hash_hmac('sha512', $password, '#+pT%B[M1X3R),Z(Y+q_tZwX~l@DXum2PdHS8hvmaVENrmI_?cvE#j8]n^.u]Ni+');
+	$password = hash_hmac('sha512', $password, '#+pT%B[M1X3R),Z(Y+q_tZwX~l@DXum2PdHS8hvmaVENrmI_?cvE#j8]n^.u]Ni+');
 
-    $stmt = $db->prepare("SELECT * FROM user WHERE username='$username' AND password='$password'");
-    $stmt->execute();
-    $user = $stmt->fetch();
+	$stmt = $db->prepare("SELECT * FROM user WHERE username='$username' AND password='$password'");
+	$stmt->execute();
+	$user = $stmt->fetch();
 
-    if($password == $user['password']) {
+	if($password == $user['password']) {
 		// Store the data in the session
 		$_SESSION['username'] = $username;
 		$_SESSION['rcrypt'] = hash_hmac('sha512', $_SESSION['username'], '#+pT%B[M1X3R),Z(Y+q_tZwX~l@DXum2PdHS8hvmaVENrmI_?cvE#j8]n^.u]Ni+');
